3ace18346ea79fa3e8a356c1c2702f77946a14f5
Cameron Tredoux <camtredoux@gmail.com>
2021-12-27 12:10:12 -0800

    update post resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -5,13 +5,13 @@ import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() ctx: MyContext) {
-    return ctx.em.find(Post, {});
+  async posts(@Ctx() ctx: MyContext) {
+    return await ctx.em.find(Post, {});
   }
 
   @Query(() => Post, { nullable: true })
-  post(@Arg("_id") _id: number, @Ctx() ctx: MyContext) {
-    return ctx.em.findOne(Post, { _id });
+  async post(@Arg("_id") _id: number, @Ctx() ctx: MyContext) {
+    return await ctx.em.findOne(Post, { _id });
   }
 
   @Mutation(() => Post)
@@ -24,6 +24,25 @@ export class PostResolver {
   ) {
     const post = ctx.em.create(Post, { title, name, _id, email });
     await ctx.em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") _id: number,
+    @Arg("title") title: string,
+    @Ctx() ctx: MyContext
+  ) {
+    const post = await ctx.em.findOne(Post, { _id });
+    if (!post) {
+      return null;
+    }
+
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await ctx.em.persistAndFlush(post);
+    }
+
     return post;
   }
 }
