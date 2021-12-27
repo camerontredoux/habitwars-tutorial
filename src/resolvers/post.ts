00ded00f15ded1cc299fa4d0dcb488f43b8de82c
Cameron Tredoux <camtredoux@gmail.com>
2021-12-27 13:56:11 -0800

    return types on posts resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -5,12 +5,15 @@ import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(@Ctx() ctx: MyContext) {
+  async posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     return await ctx.em.find(Post, {});
   }
 
   @Query(() => Post, { nullable: true })
-  async post(@Arg("_id") _id: number, @Ctx() ctx: MyContext) {
+  async post(
+    @Arg("_id") _id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<Post | null> {
     return await ctx.em.findOne(Post, { _id });
   }
 
@@ -21,7 +24,7 @@ export class PostResolver {
     @Arg("id") _id: number,
     @Arg("email") email: string,
     @Ctx() ctx: MyContext
-  ) {
+  ): Promise<Post> {
     const post = ctx.em.create(Post, { title, name, _id, email });
     await ctx.em.persistAndFlush(post);
     return post;
@@ -32,7 +35,7 @@ export class PostResolver {
     @Arg("id") _id: number,
     @Arg("title") title: string,
     @Ctx() ctx: MyContext
-  ) {
+  ): Promise<Post | null> {
     const post = await ctx.em.findOne(Post, { _id });
     if (!post) {
       return null;
@@ -44,5 +47,14 @@ export class PostResolver {
     }
 
     return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") _id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<boolean> {
+    await ctx.em.nativeDelete(Post, { _id });
+    return true;
   }
 }
