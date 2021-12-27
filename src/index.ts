ce600716f1e2262c3deb7b48dd42de3765f23b42
Cameron Tredoux <camtredoux@gmail.com>
2021-12-27 01:04:52 -0800

    added test resolver and setting up graphql and apollo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/post";
 import config from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(config);
   await orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
     console.log(`Server started at http://localhost:4000`);
   });
